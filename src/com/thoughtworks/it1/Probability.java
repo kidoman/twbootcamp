ba0a6e0905f868a4a6f936446284ee68d3757007
Karan Misra <karan@erodov.com>
2012-02-28 16:22:04 +0530

    Added .or() and constructor guard clauses

--- src/com/thoughtworks/it1/Probability.java
+++ src/com/thoughtworks/it1/Probability.java
@@ -3,16 +3,16 @@ package com.thoughtworks.it1;
 /* Chance of an event happening */
 public class Probability {
     private static final double CERTAIN_PROBABILITY = 1.0;
-    private static final double NO_PROBABILITY = 0.0;
+    private static final double IMPOSSIBLE_PROBABILITY = 0.0;
 
     public static final Probability Certainty = new Probability(CERTAIN_PROBABILITY);
-    public static final Probability Never = new Probability(NO_PROBABILITY);
+    public static final Probability Never = new Probability(IMPOSSIBLE_PROBABILITY);
 
     private final double value;
 
     public Probability(double value) throws ArithmeticException {
         if (value < 0 || value > 1)
-            throw new ArithmeticException("Probability value cannot be < 0 or > 1");
+            throw new IllegalArgumentException("Probability value cannot be < 0 or > 1");
 
         this.value = value;
     }
@@ -21,16 +21,21 @@ public class Probability {
         return new Probability(CERTAIN_PROBABILITY - value);
     }
 
-    public Probability and(Probability probability) {
-        return new Probability(value * probability.value);
+    public Probability and(Probability another) {
+        return new Probability(value * another.value);
+    }
+
+    public Probability or(Probability other) {
+        return not().and(other.not()).not();
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (object == this) return true;
-        if (object == null) return false;
-        if (object instanceof Probability)
-            return value == ((Probability) object).value;
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other instanceof Probability)
+            return value == ((Probability) other).value;
         return false;
     }
+
 }
--- src/com/thoughtworks/test/ProbabilityTest.java
+++ src/com/thoughtworks/test/ProbabilityTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProbabilityTest {
-    @Test(expected = ArithmeticException.class)
-    public void throwsIfNegativeProbabilityValueGiven() {
+    @Test(expected = IllegalArgumentException.class)
+    public void probabilityCannotBeNegative() {
         p(-0.1);
     }
     
-    @Test(expected = ArithmeticException.class)
-    public void throwsIfGt1ProbabilityValueGiven() {
+    @Test(expected = IllegalArgumentException.class)
+    public void probabilityCannotExceedUnity() {
         p(1.1);
     }
     
@@ -27,12 +27,12 @@ public class ProbabilityTest {
     }
     
     @Test
-    public void canProvideProbabilityOfTwoEventsOccuringTogether() {
+    public void canProvideProbabilityOfTwoMutuallyExclusiveEventsOccuringTogether() {
         assertEquals(p(0.06), p(0.2).and(p(0.3)));
     }
 
     @Test
-    public void canProviderProbabilityOfACertainConditionAndAConditionWhichWouldNeverOccur() {
+    public void canProvideProbabilityOfACertainEventAndANeverEventOccuringTogether() {
         assertEquals(Probability.Never, Probability.Certainty.and(Probability.Never));
     }
     
@@ -50,17 +50,22 @@ public class ProbabilityTest {
     public void probabilityEqualsSelf() {
         Probability p = p(0.1);
         
-        assertTrue(p.equals(p));
+        assertEquals(p, p);
     }
 
     @Test
     public void twoEventsWithEqualChancesOfOccuranceAreEqual() {
-        assertTrue(p(0.1).equals(p(0.1)));
+        assertEquals(p(0.1), p(0.1));
     }
     
     @Test
     public void twoEventsWithDifferentChancesOfOccuranceAreNotEquallyProbable() {
         assertFalse(p(0.1).equals(p(0.2)));
+    }
+
+    @Test
+    public void canProvideProbabilityOfEitherEventOccurring(){
+        assertEquals(p(0.52), p(0.4).or(p(0.2)));
     }
 
     private Probability p(double value) {
